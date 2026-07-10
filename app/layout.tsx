f746fe8b04e67f336f2cea9e5a578d6064bdfd2f
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2026-07-10 08:14:47 +0100

    There some issues with tailwind classes , require to recheck:45:09

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { getDocuments} from "@/lib/doc";
+import { getDocuments } from "@/lib/doc";
 import Header from "@/components/Header";
 
 const geistSans = Geist({
@@ -31,7 +31,21 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body>
+        <div className="h-full lg:ml-72 xl:ml-80">
+          <Header />
+          <div className="relative px-4 pt-14 sm:px-6 lg:px-8">
+            <main className="flex-auto py-16">
+              <div className="absolute inset-0 -z-10 mx-0 max-w-none overflow-hidden">
+                <div className="absolute left-1/2 top-0 ml-[-38rem] h-[25rem] w-[81.25rem] dark:[mask-image:linear-gradient(white,transparent)]">
+                  <div className="absolute inset-0 bg-gradient-to-r from-[#36b49f] to-[#DBFF75] opacity-40 [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] dark:from-[#36b49f]/30 dark:to-[#DBFF75]/30 dark:opacity-100"></div>
+                </div>
+              </div>
+              {children}
+            </main>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
