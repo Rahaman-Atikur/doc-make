846300c536ce0a5f5c4c624d57479247b2aa2812
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2026-07-14 12:19:47 +0100

    Sidebar Dynamic routing link changing in url and its working-1.00:56

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 type Doc = {
   id: string;
@@ -14,14 +15,36 @@ type SidebarProps = {
 };
 
 const Sidebar = ({ docs }: SidebarProps) => {
-  const root = docs.filter((doc) => !doc.parent);
-  console.log({root}); 
-  const nonRoots = Object.groupBy(docs.filter((doc) => doc.parent), ({parent})=>parent);
-  console.log({nonRoots});
+  const roots = docs.filter((doc) => !doc.parent);
+  console.log({ roots });
+  const nonRoots = Object.groupBy(
+    docs.filter((doc) => doc.parent),
+    ({ parent }) => parent,
+  );
+  console.log({ nonRoots });
 
-
-
-  return <div>Sidebar</div>;
+  return (
+    <nav className="lg:block my-10">
+      <ul>
+        <div className="absolute inset-x-0 top-0 bg-zinc-800/2.5 will-change-transform dark:bg-white/2.5"></div>
+        <div className="absolute inset-y-0 left-2 w-px bg-zinc-900/10 dark:bg-white/5"></div>
+        <div className="absolute left-2 h-6 w-px bg-emerald-500"></div>
+        <ul role="list" className="border-l border-transparent">
+          {roots.map((rootNode) => (
+            <li key={rootNode.id} className="relative">
+              <Link
+                aria-current="page"
+                className="flex justify-between gap-2 py-1 pl-4 pr-3 text-sm text-zinc-900 transition dark:text-white"
+                href={`/docs/${rootNode.id}`}
+              >
+                <span className="truncate">{rootNode.title}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </ul>
+    </nav>
+  );
 };
 
 export default Sidebar;
